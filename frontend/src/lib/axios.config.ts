62c13ddf9f22f8c4d9c492b92e651588d1909344
Anadudev <anadudev@gmail.com>
2025-03-16 02:02:10 +0100

    feat: update axios configuration to use environment-based baseURL for development and production

--- frontend/src/lib/axios.config.ts
+++ frontend/src/lib/axios.config.ts
@@ -2,8 +2,11 @@ import axios from "axios";
 
 // todo handle axios timeouts
 const axiosInstance = axios.create({
-  // baseURL: process.env.MODE === "development" ? "http://localhost:5000/" : "/api",
-  baseURL: "http://localhost:5000/",
+  baseURL:
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:5000/"
+      : "https://aireader.onrender.com/",
+  // baseURL: "http://localhost:5000/",
   timeout: 5000,
 });
 
