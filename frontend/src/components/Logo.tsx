bdbebd3a00e4590446885bff7915a14f168d6686
Anadudev <anadudev@gmail.com>
2025-03-16 02:49:50 +0100

    feat: enhance Logo component with animated image and layout adjustments

--- frontend/src/components/Logo.tsx
+++ frontend/src/components/Logo.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
@@ -9,10 +10,19 @@ type Props = {
 const Logo: React.FC<Props> = ({ className, href }) => {
   return (
     <Link
-      className={`text-3xl font-bold tracking-tight bg-gradient-to-r from-indigo-500 bg-clip-text text-transparent to-pink-500 ${className}`}
+      className={`flex text-3xl font-bold tracking-tight bg-gradient-to-r from-indigo-500 bg-clip-text text-transparent to-pink-500 ${className}`}
       href={href ?? "/"}
     >
       AIreader
+      <div className="absolute size-3 animate-spin top-1 left-[2.2rem] transition-all duration-1500">
+        <Image
+          className="rounded-full group-hover:animate-spin transition-all duration-200"
+          title="Ai logo"
+          src="/gemini_sparkle.svg"
+          alt="AI Logo"
+          fill
+        />
+      </div>
     </Link>
   );
 };
