7308dc1b190586e2fc1f71b556c26f85e337760b
Anadudev <anadudev@gmail.com>
2025-03-19 18:55:20 +0100

    refactor: adjust axios baseURL configuration for better environment handling

--- frontend/src/lib/axios.config.ts
+++ frontend/src/lib/axios.config.ts
@@ -2,11 +2,11 @@ import axios from "axios";
 
 // todo handle axios timeouts
 const axiosInstance = axios.create({
-  // baseURL:
-  //   process.env.NODE_ENV === "development"
-  //     ? "http://localhost:5000/"
-  //     : "https://aireader.onrender.com/",
-  baseURL: "http://localhost:5000/",
+  baseURL:
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:5000/"
+      : "https://aireader.onrender.com/",
+  // baseURL: "http://localhost:5000/",
   timeout: 5000,
 });
 
