ce87c40a5fb73dceeede07713effe99793511cb6
Anadudev <anadudev@gmail.com>
2025-03-20 11:35:55 +0100

    feat: update development script to reset cli before starting and enhance FeatureCard styling for dark mode

--- frontend/src/components/FeatureCard.tsx
+++ frontend/src/components/FeatureCard.tsx
@@ -3,12 +3,14 @@ import { FeatureType } from "@/types/feature";
 
 const FeatureCard: React.FC<FeatureType> = ({ title, description, Icon }) => {
   return (
-    <div className="flex-1 sm:min-w-[20rem] group cursor-default flex flex-col items-center gap-4 sm:gap-6 bg-blur sm:max-w-xl mx-auto p-4 sm:p-6 rounded-2xl hover:shadow-md">
+    <div className="flex-1 sm:min-w-[20rem] group cursor-default flex flex-col items-center gap-4 sm:gap-6 bg-blur sm:max-w-xl mx-auto p-4 sm:p-6 rounded-2xl hover:shadow-md dark:hover:bg-slate-800">
       <Icon size="2rem" className=" text-indigo-500" />
       <h3 className="text-xl font-semibold text-center group-hover:bg-gradient-to-r from-indigo-500 bg-clip-text group-hover:text-transparent to-pink-500 transition-all ease-in-out duration-400">
         {title}
       </h3>
-      <p className="text-gray-600 text-center">{description}</p>
+      <p className="text-gray-600 text-center dark:group-hover:text-gray-400">
+        {description}
+      </p>
     </div>
   );
 };
--- frontend/src/lib/axios.config.ts
+++ frontend/src/lib/axios.config.ts
@@ -1,12 +1,13 @@
 import axios from "axios";
 
 // todo handle axios timeouts
+// todo handle slow internat detection
 const axiosInstance = axios.create({
-  baseURL:
-    process.env.NODE_ENV === "development"
-      ? "http://localhost:5000/"
-      : "https://aireader.onrender.com/",
-  // baseURL: "http://localhost:5000/",
+  // baseURL:
+    // process.env.NODE_ENV === "development"
+    //   ? "http://localhost:5000/"
+    //   : "https://aireader.onrender.com/",
+  baseURL: "http://localhost:5000/",
   timeout: 5000,
 });
 
