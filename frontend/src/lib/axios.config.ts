34ce364c103eb1861418823f0cde37775e0b710d
Anadudev <anadudev@gmail.com>
2025-03-24 01:57:48 +0100

    feat: enable credentials for Axios requests

--- frontend/src/lib/axios.config.ts
+++ frontend/src/lib/axios.config.ts
@@ -6,6 +6,7 @@ const axiosInstance = axios.create({
   baseURL: "https://aireader.onrender.com/",
   // baseURL: "http://localhost:5000/",
   timeout: 5000,
+  withCredentials: true,
 });
 
 export default axiosInstance;
