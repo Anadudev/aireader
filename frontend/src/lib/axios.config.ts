92bd70566c6ac0aaec622f6b43d417983f17d269
Anadudev <anadudev@gmail.com>
2025-03-24 09:50:04 +0100

    feat: update Axios base URL for production environment

--- frontend/src/lib/axios.config.ts
+++ frontend/src/lib/axios.config.ts
@@ -3,8 +3,8 @@ import axios from "axios";
 // todo handle axios timeouts
 // todo handle slow internat detection
 const axiosInstance = axios.create({
-  // baseURL: "https://aireader.onrender.com/",
-  baseURL: "http://localhost:5000/",
+  baseURL: "https://aireader.onrender.com/",
+  // baseURL: "http://localhost:5000/",
   timeout: 5000,
   withCredentials: true,
 });
