f89c3f21978631e1a2508d5a7a2ebec7235e3bb6
Anadudev <anadudev@gmail.com>
2025-04-01 21:43:28 +0100

    fix: update axios base URL to use process.env for environment detection

--- frontend/src/lib/axios.config.ts
+++ frontend/src/lib/axios.config.ts
@@ -4,9 +4,7 @@ import axios from "axios";
 // todo handle slow internat detection
 const axiosInstance = axios.create({
   baseURL:
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-expect-error
-    import.meta.env.MODE === "development"
+    process.env.MODE === "development"
       ? "http://localhost:5000/api"
       : "https://aireader.onrender.com/",
   timeout: 5000,
