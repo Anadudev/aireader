d3955c76feec1c6945819fdfc7c491fbf8728d6a
Anadudev <anadudev@gmail.com>
2025-04-26 15:57:14 +0100

    feat: add loading state to EmptyData component for improved user experience

--- frontend/src/components/EmptyData.tsx
+++ frontend/src/components/EmptyData.tsx
@@ -1,5 +1,6 @@
+"use client";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 
 declare type EmptyDataProps = {
   className?: string;
@@ -16,28 +17,33 @@ const EmptyData: React.FC<EmptyDataProps> = ({
   link,
   href,
 }) => {
+  const [load, setLoad] = useState(false);
+  setTimeout(() => setLoad(true), 1000);
+
   return (
-    <div className={`flex items-center justify-center ${className}`}>
-      {body ? (
-        body
-      ) : (
-        <div className="">
-          <div className="flex items-center justify-center">
-            <div className="text-center">
-              <h2 className="mb-3 text-3xl font-bold text-gray-900 dark:text-white">
-                {message || "No Data"}
-              </h2>
-              <Link
-                href={href || "/"}
-                className="text-blue-600 hover:underline"
-              >
-                {link || " Go to Home"}
-              </Link>
+    load && (
+      <div className={`flex items-center justify-center ${className}`}>
+        {body ? (
+          body
+        ) : (
+          <div className="">
+            <div className="flex items-center justify-center">
+              <div className="text-center">
+                <h2 className="mb-3 text-3xl font-bold text-gray-900 dark:text-white">
+                  {message || "No Data"}
+                </h2>
+                <Link
+                  href={href || "/"}
+                  className="text-blue-600 hover:underline"
+                >
+                  {link || " Go to Home"}
+                </Link>
+              </div>
             </div>
           </div>
-        </div>
-      )}
-    </div>
+        )}
+      </div>
+    )
   );
 };
 
